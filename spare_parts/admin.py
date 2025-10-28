48f9b967f88c3ae976f9ae36b96969d335efc241
WRXer <wrxerrr@gmail.com>
2025-10-28 09:36:36 +0400

    Добавил в админку модели из spare_parts

--- spare_parts/admin.py
+++ spare_parts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from spare_parts.models import Part, CarGeneration, CarMake, CarModel, Category
+
+
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('make', 'name')
+
+@admin.register(CarGeneration)
+class CarGenerationAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'year_start', 'year_end')
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'price', 'donor_generation', 'condition', 'is_active', 'created_at'
+    )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
