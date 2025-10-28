d0e7529d9842920d7b07e077e3bc7da8a405b5d7
WRXer <wrxerrr@gmail.com>
2025-10-28 19:13:53 +0400

    Добавил урлы в spare_parts

--- spare_parts/urls.py
+++ spare_parts/urls.py
@@ -1,7 +1,4 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+"""The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
@@ -14,11 +11,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('', include('spare_parts.urls'))
+    path('', views.index, name='index'),
+    path('catalog/', views.about, name='catalog'),
 ]
