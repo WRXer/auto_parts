a10c0f4005d860f2b4fd559aae6e9e42756af80d
WRXer <wrxerrr@gmail.com>
2025-10-30 19:31:28 +0400

    Заготовка под страницу оплата + привяз урл доставка

--- main/urls.py
+++ main/urls.py
@@ -19,4 +19,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contacts/', views.contacts, name='contacts'),
-]
+    path('delivery/', views.contacts, name='delivery'),
+    path('payment/', views.contacts, name='payment'),
+    ]
