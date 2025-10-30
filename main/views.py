532b4bf95ffdb0196dcc16cb406d9b679cbbc5ae
WRXer <wrxerrr@gmail.com>
2025-10-30 17:29:40 +0400

    Заготовка под раздел О нас Также добавил ссылку на значек - главная страница

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("About site")
+    return render(request, 'main/about.html')
 
 def contacts(request):
     return render(request, 'main/contacts.html')
