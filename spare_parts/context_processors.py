b0ff8f788b39d131f1dcf28078dae5e202f6a405
WRXer <wrxerrr@gmail.com>
2025-11-12 13:39:32 +0400

    Исправил открытие категорий в шапке

--- spare_parts/context_processors.py
+++ spare_parts/context_processors.py
@@ -1,10 +1,10 @@
 from .models import Category
 
 
-def categories(request):
+def all_categories(request):
     """
     Добавляет список всех активных категорий в контекст всех шаблонов.
     """
     return {
-        'categories': Category.objects.all().order_by('name')
+        'all_categories': Category.objects.all().order_by('name')
     }
