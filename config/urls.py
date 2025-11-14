c50e4e9d3b6a9e20cca7d549facb2f74e7f8ba76
WRXer <wrxerrr@gmail.com>
2025-11-14 12:50:24 +0400

    Урл users

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('spare_parts.urls', namespace='spare_parts')),
+    path('', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
