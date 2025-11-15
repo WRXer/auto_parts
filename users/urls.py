6ad551d10b1da04097a7524868d81911828806e5
WRXer <wrxerrr@gmail.com>
2025-11-15 12:08:17 +0400

    Страница профиля + смены пароля

--- users/urls.py
+++ users/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration_page'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('password/change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change_form.html'), name='password_change'),
+    path('password/change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
