aa460f9ca97fef18470185c44aa0904df47c5b4a
WRXer <wrxerrr@gmail.com>
2025-12-03 10:57:08 +0400

    Поправил валидацию при регистрации нового пользователя

--- users/forms.py
+++ users/forms.py
@@ -7,6 +7,25 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ('email', 'phone', 'first_name', 'last_name')
 
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        phone = cleaned_data.get('phone')
+
+        if not email:    #Если email пустой, ошибка уже должна быть в поле email, но проверим.
+            self.add_error('email', 'Email является обязательным полем для регистрации.')
+
+        # АЛЬТЕРНАТИВНУЮ регистрация через телефон,
+        # if not email and not phone:
+        #     raise forms.ValidationError(
+        #         "Для регистрации необходимо указать либо Email, либо Номер телефона."
+        #     )
+
+        for field in self.fields:
+            if self.fields[field].widget.__class__.__name__ not in ('CheckboxInput', 'ClearableFileInput'):
+                self.fields[field].widget.attrs['class'] = 'form-control'
+        return cleaned_data
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
